3078da1badacdde5fc002185aa25e659c9893b9e
AZapie <ajzapien03@gmail.com>
2024-10-11 10:48:03 -0500

    Updated footer links

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,3 +1,4 @@
+// biome-ignore lint/style/useImportType: <explanation>
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
 
@@ -7,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/BengalBots-LSU",
+      "Discord Community": "https://discord.com/invite/f9rmbGcU4y",
     },
   }),
 }
